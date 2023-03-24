3bf5bff32d7175ae00f6a92283d52908e9c2d10d
MrH-rezroll <mhodge8414@gmail.com>
2023-03-23 18:02:31 -0700

    Adventure game completion point for 03.23.23

--- Phase-1/032323/scripts.js
+++ Phase-1/032323/scripts.js
@@ -2,6 +2,35 @@ var consoleContent = document.getElementById("console-output");
 
 var isPoweredOn = false;
 
+var posX = 0;
+var posY = 0;
+
+function moveUpOnY(){
+    posY++;
+    outputPosition();
+    checkLocation();
+}
+function moveDownOnY(){
+    posY--;
+    outputPosition();
+    checkLocation();
+}
+
+function moveRightOnX(){
+    posX++;
+    outputPosition();
+    checkLocation();
+}
+function moveLeftOnX(){
+    posX--;
+    outputPosition();
+    checkLocation();
+}
+
+function outputPosition(){
+    return "PosX " + posX + " : PosY " + posY;
+}
+
 function power(){
     if (!isPoweredOn){
         consoleContent.innerHTML = "Welcome to the Adventure Game!";
@@ -10,5 +39,20 @@ function power(){
     else {
         consoleContent.innerHTML = "";
         isPoweredOn = false;
+    }
+}
+
+console.log("hello console");
+
+function checkLocation(){
+    consoleContent.innerHTML = outputPosition() + "<br>";
+    if(posX === 0 && posY === 0){
+        consoleContent.innerHTML += "You are at the start.";
+    }
+    else if (posX === -4 && posY === 7) {
+        consoleContent.innerHTML += "You found the treasure chest!";
+    }
+    else{
+        consoleContent.innerHTML += "There's nothing here.";
     }
 }
